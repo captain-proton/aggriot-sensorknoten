ffaf637d28646510c4477634cef25da1bcf71eef
Nils Verheyen <nverheyen@gmail.com>
2017-10-12 09:29:42 +0200

    fixed sample time, added documentation

--- src/main.ino
+++ src/main.ino
@@ -15,26 +15,45 @@ Grove - Dust Sensor Demo v1.0
 int pin = 8;
 unsigned long duration;
 unsigned long starttime;
-unsigned long sampletime_ms = 2000;//sampe 30s&nbsp;;
+unsigned long sampletime_ms = 30000;
 unsigned long lowpulseoccupancy = 0;
 float ratio = 0;
 float concentration = 0;
 
+/*
+Take a look at the arduino reference page for detailed function
+descriptions.
+
+https://www.arduino.cc/en/Reference/HomePage
+*/
+
+// start of the sketch
 void setup()
 {
+    // data rate in bits per second for serial transmission
     Serial.begin(9600);
-    pinMode(8, INPUT);
-    starttime = millis();//get the current time;
+
+    // read from pin 8
+    pinMode(pin, INPUT);
+
+    // millis() returns 0 for arduino start time
+    starttime = millis();
 }
 
+// called after setup(). loops consecutively. there is not guarantee that
+// it is called in constant gaps
 void loop()
 {
+    // how long the pin had a low pulse block until it got HIGH (microseconds!)
     duration = pulseIn(pin, LOW);
+
+    // add duration to the current occupancy
     lowpulseoccupancy = lowpulseoccupancy + duration;
 
-    if ((millis() - starttime) >= sampletime_ms)//if the sampel time = = 30s
+    // low pulse is measured until sample time is reached
+    if ((millis() - starttime) >= sampletime_ms)
     {
-        // Integer percentage 0=&gt;100
+        // Integer percentage 0=>100
         ratio = lowpulseoccupancy / (sampletime_ms * 10.0);
 
         // using spec sheet curve
@@ -44,6 +63,8 @@ void loop()
         Serial.print(concentration);
         Serial.println(" pcs/0.01cf");
         Serial.println("\n");
+
+        // reset values to start sampling again
         lowpulseoccupancy = 0;
         starttime = millis();
     }
