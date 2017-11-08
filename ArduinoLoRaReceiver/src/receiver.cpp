e867bb3b624f13c7d14adec0111ffbd337bb22dc
Nils Verheyen <nils.verheyen@uni-due.de>
2017-11-08 14:28:36 +0100

    added send of response

--- ArduinoLoRaReceiver/src/receiver.cpp
+++ ArduinoLoRaReceiver/src/receiver.cpp
@@ -9,28 +9,18 @@ Based on:
 #include <SPI.h>
 #include <RH_RF95.h>
 
+#define SERIAL_MAX_READ             256
+
 // Singleton instance of the radio driver
 RH_RF95 driver;
+
+uint8_t input[SERIAL_MAX_READ];
+int inputLen;
 
 /* the led is used to highlight if a message is received. */
 uint8_t led = LED_BUILTIN;
 
-void setup() {
-
-    pinMode(led, OUTPUT);
-    Serial.begin(9600);
-    // Wait for serial port to be available
-    while (!Serial) ;
-
-    if (!driver.init()) {
-        Serial.println(F("Init failed"));
-    } else {
-        driver.setFrequency(433);
-    }
-
-}
-
-void loop() {
+void checkRadio() {
 
     if (driver.available()) {
         // Dont put this on the stack:
@@ -45,5 +35,63 @@ void loop() {
 
             digitalWrite(led, LOW);
         }
+    }
+}
+
+void clearInput() {
+    for (int i = 0; i < SERIAL_MAX_READ; i++) {
+        input[i] = 0;
+    }
+    inputLen = 0;
+}
+
+bool checkInput() {
+    uint8_t available = Serial.available();
+    if (available > 0) {
+        if (inputLen + available >= SERIAL_MAX_READ)
+            clearInput();
+
+        input[inputLen] = Serial.read();
+        inputLen++;
+
+        if (input[inputLen - 1] == '\n') {
+            return true;
+        }
+    }
+    return false;
+}
+
+void sendResponse() {
+
+    digitalWrite(led, HIGH);
+
+    driver.send(input, inputLen);
+    driver.waitPacketSent();
+    driver.setModeRx();
+
+    clearInput();
+
+    digitalWrite(led, LOW);
+}
+
+void setup() {
+
+    pinMode(led, OUTPUT);
+    Serial.begin(9600);
+    // Wait for serial port to be available
+    while (!Serial) ;
+
+    if (!driver.init()) {
+        Serial.println(F("Init failed"));
+    } else {
+        driver.setFrequency(433);
+    }
+}
+
+void loop() {
+
+    checkRadio();
+    if (checkInput()) {
+        sendResponse();
     }
 }
